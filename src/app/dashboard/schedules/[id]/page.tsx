246e6eae0bb7b6729bb745a565d5147453cbfb1f
Antonio-Claudio Andelic <andelic.antonio.claudio91@gmail.com>
2026-06-03 12:40:32 +0200

    Add assignment link to schedule details

--- src/app/dashboard/schedules/[id]/page.tsx
+++ src/app/dashboard/schedules/[id]/page.tsx
@@ -46,6 +46,12 @@ export default async function ScheduleDetailsPage({
 
         <div className="flex gap-2">
           <Link
+            href="/dashboard/schedule-assignments"
+            className="rounded bg-blue-600 px-4 py-2 text-white">
+            Add Assignment
+          </Link>
+
+          <Link
             href={`/dashboard/schedules/${schedule.id}/edit`}
             className="rounded bg-gray-800 px-4 py-2 text-white">
             Edit
